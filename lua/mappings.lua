f7ec245a79ca98bb1d5091abf5fcc70effd70bc5
me7alix <me7lix@gmail.com>
2025-05-30 13:06:50 +0300

    paste doesnt copy previous text

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,8 @@
 require "nvchad.mappings"
 
 local map = vim.keymap.set
-
+map({ "v", "x" }, "p", [["_dP]], { silent = true })
+map({ "v", "x" }, "P", [["_dp]], { silent = true })
 map("n", "d", '"_d')
 map("v", "d", '"_d')
 map("n", ";", ":", { desc = "CMD enter command mode" })
